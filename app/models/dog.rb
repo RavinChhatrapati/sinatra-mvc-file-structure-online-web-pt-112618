baecba9a65725bd76bbbcd3eccaff34f332e830c
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-14 01:27:17 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,8 @@ class Dog
 @@all = []
 attr_accessor :name, :age, :breed
 def initialize(name, age, breed)
-  @name = name
-  @age = age
-  @breed = breed
 
+@@all << self
 end
   def self.all
   @@all
